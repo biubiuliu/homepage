703a03f156ff49b360641135381e4a21cf9e0695
biubiuliu <172117859@qq.com>
2019-09-07 17:45:03 +0800

    '背景图片修改'

--- src/components/card/barCard.vue
+++ src/components/card/barCard.vue
@@ -1,18 +1,51 @@
-<template lang="zh">
-  <div>
-    barcard
+<template>
+  <div class="card_body">
+    <div class="bar_card" v-for="(item,index) in cardData" :key="index" v-on:click="handCardRouter(item)" >
+      <div class="bar_card_box">
+        <img class="bar_card_img" :src="item.img" alt="图片不存在">
+        <div class="bar_card_title">{{item.title}}</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'barcard',
+  props: {
+    cardData: Array
+  },
   data () {
     return {
 
+    }
+  },
+  methods: {
+    handCardRouter (item) {
+      this.$emit('handCardRouter', item)
     }
   }
 }
 </script>
 <style scope>
+.card_body{
+  display: flex;
+  justify-content: flex-start;
+  cursor: pointer;
+  font-size: 12px;
 
+}
+.bar_card_box{
+  margin: 10px;
+  background: white;
+}
+.bar_card_img{
+  width: 100%
+}
+.bar_card_title{
+  font-size: 200%;
+  color: #475164
+}
+.bar_card{
+  width: 40%
+}
 </style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -23,19 +23,98 @@
             <button id="searchBtn" class="searchBtn"></button>
         </form>
     </header>
+    <div id="main">
+      <bar-card @handCardRouter='handCardRouter' :cardData='cardData'></bar-card>
+    </div>
   </div>
 </template>
 
 <script>
+import BarCard from './card/barCard'
 export default {
+  components: {
+    BarCard
+  },
   name: 'home',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      ifSwitch: false
+      ifSwitch: false,
+      ifInputting: false,
+      cardData: [
+        {
+          title: '百度',
+          url: 'baidu',
+          img: 'https://cdn.jsdelivr.net/npm/@bootcss/www.bootcss.com@0.0.3/dist/img/babeljs.png'
+        },
+        {
+          title: '谷歌',
+          url: 'google',
+          img: 'https://cdn.jsdelivr.net/npm/@bootcss/www.bootcss.com@0.0.3/dist/img/jqueryapi.png'
+        },
+        {
+          title: '谷歌',
+          url: 'google',
+          img: 'https://cdn.jsdelivr.net/npm/@bootcss/www.bootcss.com@0.0.3/dist/img/jqueryapi.png'
+        },
+        {
+          title: '谷歌',
+          url: 'google',
+          img: 'https://cdn.jsdelivr.net/npm/@bootcss/www.bootcss.com@0.0.3/dist/img/jqueryapi.png'
+        },
+        {
+          title: '斗鱼',
+          url: '斗鱼',
+          img: 'https://cdn.jsdelivr.net/npm/@bootcss/www.bootcss.com@0.0.3/dist/img/babeljs.png'
+        }
+      ]
+
     }
   },
+  mounted () {
+    this.listenToUser()
+  },
   methods: {
+    listenToUser () {
+      // ifInputting作为一个开关
+      this.ifInputting = false
+      let inputBar = document.getElementById('inputBar')
+      let searchBtn = document.querySelector('.searchBtn')
+      inputBar.addEventListener('focus', function (e) {
+        this.ifInputting = true
+        e.target.placeholder = ''
+      })
+      inputBar.addEventListener('focusout', function (e) {
+        this.ifInputting = false
+        let searchEnginLogo = document.getElementById('searchEnginLogo')
+        let engin = searchEnginLogo.getAttribute('data-engin')
+        e.target.placeholder = '点击左边图标切换搜索引擎'
+        switch (engin) {
+          case 'baidu':
+            e.target.placeholder = '众里寻他千百度，蓦然回首，那人却在灯火阑珊处'
+            break
+          case 'google':
+            e.target.placeholder = '昨夜西风凋碧树，独上高楼，望尽天涯路'
+            break
+        }
+      })
+      searchBtn.onclick = function (e) {
+        e.preventDefault()
+        let searchContent = inputBar.value
+        // 判断是什么搜索引擎
+        let searchEnginLogo = document.getElementById('searchEnginLogo')
+        let engin = searchEnginLogo.getAttribute('data-engin')
+
+        switch (engin) {
+          case 'baidu':
+            window.open('https://www.baidu.com/s?wd=' + searchContent, '_blank')
+            break
+          case 'google':
+            window.open('https://www.google.com.hk/search?q=' + searchContent, '_blank')
+            break
+        }
+      }
+    },
     switchSearchEngin () {
     // 搜索引擎默认是google
       let searchEnginLogo = document.getElementById('searchEnginLogo')
@@ -66,6 +145,9 @@ export default {
         this.ifSwitch = !this.ifSwitch
         console.log(this.ifSwitch)
       }
+    },
+    handCardRouter (item) {
+      console.log(item)
     }
   }
 }
@@ -156,5 +238,11 @@ header .searchBar .searchEnginLogo li.active{
     border: none;
     outline: none;
 }
-
+#main{
+  border: 1px solid red;
+  width: 80%;
+  height: 500px;
+  margin: 0 auto;
+  padding: 10px
+}
 </style>
